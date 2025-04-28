2f70af3e8e04fb97988eaf27e42d3fe7b4b18678
E1B49 <u11302249@o365.ttu.edu.tw>
2025-04-28 17:56:35 +0800

    second commit 第二次讓使用者輸入密碼

--- P2E1B49.cpp
+++ P2E1B49.cpp
@@ -35,3 +35,16 @@ printf("                           \n");
     printf("|   ============          |\n");
     printf("                           \n"); 
     printf("---------------------------\n");
+int mima,i=0;
+while(1)
+{
+i++;
+scanf("%d",&mima);
+if(mima!=2025){printf("輸入錯誤,請重新輸入\n");}
+else
+break;
+if(i==3)return 0;
+}
+system("CLS");
+fflush(stdin);
+//第二次讓使用者輸入密碼
