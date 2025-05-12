5c1f93d74bd269ac976471f50bb454e71b120b5b
E1B49 <u11302249@o365.ttu.edu.tw>
2025-05-12 08:50:48 +0800

    first commit:做了歡迎的選單以及密碼輸入,最後是主選單

---
+++ P3E1B49.cpp
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h> 
+#include<conio.h>
+
+#define SIZE 9
+#define SPACE 10
+int main() {
+	char seats[SIZE][SIZE];  
+    for(int i=0;i<SIZE;i++)
+    {
+    	for(int j=0;j<SIZE;j++)
+    	{
+    		seats[i][j]='-';
+		}
+    }
+
+    //Step 1: Welcome screen and password
+    printf("\n");
+    printf("---------------------------\n");
+    printf("                           \n"); 
+    printf("|    /)  @  /)            |\n");
+    printf("|   (.<  U >.)            |\n");
+    printf("|   ==U==U======          |\n");
+    printf("|   |  歡   迎 |          |\n");
+    printf("|   |  光   臨 |          |\n");
+    printf("|   ============          |\n");
+printf("                           \n");   
+    printf("---------------------------\n");
+    printf("                           \n"); 
+    printf("|    /)  @  /)            |\n");
+    printf("|   (*>  u <*)            |\n");
+    printf("|   ==U==U======          |\n");
+    printf("|   |  請輸入  |          |\n");
+    printf("|   |  密  碼  |          |\n");
+    printf("|   ============          |\n");
+    printf("                           \n"); 
+    printf("---------------------------\n");
+    
+int mima,i=0;
+while(1)
+{
+i++;
+scanf("%d",&mima);
+if(mima!=2025){printf("輸入錯誤,請重新輸入\n");}
+else
+break;
+if(i==3){
+printf("錯誤訊息");
+return 0;
+}
+}
+
+//Step 2:Main Menu
+system("CLS");
+fflush(stdin);
+while(1)
+{
+printf("------------[Booking System]------------\n");
+printf("|    a.Available saets                 |\n"); 
+printf("|    b.Arrange for you                 |\n"); 
+printf("|    c.Choose by yourself              |\n"); 
+printf("|    d.Exit                            |\n"); 
+printf("----------------------------------------\n");
+
+char choose;
+scanf(" %c",&choose);
+system("CLS");
+fflush(stdin);
+}
+return 0;
+}
